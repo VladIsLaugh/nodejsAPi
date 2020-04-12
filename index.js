354730183c71a83ebfe1a86f60f0e4d6354f3bfd
Vladyslav <vladyslav.vit.kn.2017@lpnu.ua>
2020-04-12 17:20:48 +0200

    aded json

--- index.js
+++ index.js
@@ -4,50 +4,82 @@ const fs = require('fs');
 
 
 const server = http.createServer((req, res) => {
-  if (req.method === "GET") {
+    if (req.method === "GET") {
+
+        if(req.url==='/' || req.url==='/index'){
+
+            res.writeHead(200,{
+                'Content-Type' : 'text/html; charset=utf-8'
+            })
+        fs.readFile(path.join(__dirname, 'public', 'html', 'index.html'), (err, content)=>{
+                        
+            if (err) throw err
+            res.end(content )
+        })
+    }
+
+    else if(req.url==='/about'){
+        res.writeHead(200,{
+            'Content-Type' : 'text/html; charset=utf-8'
+        })
+    fs.readFile(path.join(__dirname, 'public','html', 'about.html'), (err, content)=>{
+                    
+        if (err) throw err
+        res.end(content )
+    })
+}
+
+else if(req.url==='/contacts'){
     res.writeHead(200,{
         'Content-Type' : 'text/html; charset=utf-8'
     })
-    if(req.url==='/' || req.url==='/index'){
-        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content)=>{
-                        
-            if (err) throw err
-            res.end(content )
-        })
-    }
+fs.readFile(path.join(__dirname, 'public', 'html', 'contacts.html'), (err, content)=>{
+                
+    if (err) throw err
+    res.end(content )
+})
 
-    if(req.url==='/about'){
-        fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content)=>{
-                        
-            if (err) throw err
-            res.end(content )
-        })
-    }
+}
 
-    if(req.url==='/contacts'){
-        fs.readFile(path.join(__dirname, 'public', 'contacts.html'), (err, content)=>{
-                        
-            if (err) throw err
-            res.end(content )
-        })
-    }
+else if(req.url==='/api/contacts'){
+    res.writeHead(200,{
+        'Content-Type' : 'text/json;'
+    })
+fs.readFile(path.join(__dirname, 'public', 'json', 'contacts.json'), (err, content)=>{
+                
+    if (err) throw err
+    res.end(content )
+})
 
-    else{
-        fs.readFile(path.join(__dirname, 'public', 'oops.html'), (err, content)=>{
-                        
-            if (err) throw err
-            res.end(content )
-        })
-    }
+}
 
+else{
+    if (req.method === "GET") {
+        res.writeHead(200,{
+            'Content-Type' : 'text/html; charset=utf-8'
+        })
+    fs.readFile(path.join(__dirname, 'public','html', 'oops.html'), (err, content)=>{
+                    
+        if (err) throw err
+        res.end(content )
+    })
+}
+}
+    
+    
+    
+    }else{
+        console.log('not get');
+    }
+    }
+)
 
     // res.end(`<form method='post' action='/'>
     // <input name='title' type='text' />
     //  <button type='submit'>submit</button> 
     // </form>
     //        `);
-  } 
-  
+
 //   else if (req.method === "POST") {
 //     const body = [];
 //       res.writeHead(200,{
@@ -62,7 +94,6 @@ const server = http.createServer((req, res) => {
 //       res.end(`<h1>Your message: ${message}</h1>`);
 //     });
 //   }
-});
 
 server.listen(3012, () => {
   console.log("server is running on port 3012");
